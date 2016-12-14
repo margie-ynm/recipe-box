4c437c57bd8c1db390baed2b178248030be2c046
Margie Nevarez and Skye Atwood <margie@>
2016-12-14 11:43:50 -0800

    add routing to home page and recipe form page

--- app.rb
+++ app.rb
@@ -2,3 +2,12 @@ require("bundler/setup")
 Bundler.require(:default)
 
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
+
+get('/') do
+@recipes = Recipe.all()
+erb :index
+end
+
+get('/recipes/new') do
+  erb :recipe_form
+end
