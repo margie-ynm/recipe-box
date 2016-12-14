68bf78b940fd615593571898c75dd155ab250909
Margie Nevarez and Skye Atwood <margie@>
2016-12-14 14:23:02 -0800

    implement CRUD for recipe class

--- app.rb
+++ app.rb
@@ -34,3 +34,9 @@ patch('/recipes/:id/edit') do
   @recipe.update({:name => name, :ingredients => ingredients, :instructions => instructions})
   erb :recipe
 end
+
+delete('/recipes/:id') do
+  @recipe = Recipe.find(params.fetch('id').to_i)
+  @recipe.destroy()
+  redirect '/'
+end
